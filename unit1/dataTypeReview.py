f2786624bc2a476b2593e770bf43392c6117fcc4
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-08-22 15:09:31 +0000

    took notes

---
+++ unit1/dataTypeReview.py
@@ -0,0 +1,23 @@
+print('james')
+
+#w3 SChools.com - our friend
+
+#1.
+#print each of the following data types
+#string
+print("Hello")
+print('Hello')
+#boolean
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+#integer
+x=2
+#float
+print(100.00)
+print(121.1212)
+#2
+#create and print a list that features 2 of each type.
+food = []
+
+print([True, False, 10, 100, 10.5,10.3, "I like apples", "4"])
