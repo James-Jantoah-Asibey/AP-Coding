f322233724dc71a177bc2123f40b121060848279
James-Jantoah-Asibey <jamesinfernoinc24@gmail.com>
2025-11-19 15:55:44 +0000

    did the work

---
+++ unit 3/reasearchQuestions.py
@@ -0,0 +1,10 @@
+# does defensive turnovers contribute to a teams win percentage?
+"i feel as though the questiin is not persise becquase its not talking about a specific team at all, just in general"
+"its a descriptive question because we are describing weather the defensive turnovers help the tem win or not"
+"Defensive turnovers do contribute to a team's win percentage because they disrupt the opponent's momentum and leads to points for the team that forced the turnover"
+
+
+# who has the most passing yards of all time?
+"I fell as though this question is clear because its asking us in general who has the most passing yards of all time, venevn though it could be clearer by asking to compre two different teams."
+"Its a comparitive question because its asking us who has the most passing yards of all time so we have to comapre all the players in the NFL to find the answer"
+"Tom Brady has the most passing yards of all time"
